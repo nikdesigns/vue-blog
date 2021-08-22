90c92cb4ce5a9ae3c6520580020800dce2f43a68
nikdesigns <nitinkaushikdesigns@gmail.com>
2021-08-22 11:20:22 +0530

    Blog_added

---
+++ src/components/Hero.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="pb-32">
+    <main class="lg:relative">
+      <div
+        class="mx-auto max-h-screen max-w-7xl w-full pt-16 pb-20 text-center lg:py-32 lg:text-left"
+      >
+        <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+          <h1
+            class="text-4xl tracking-tight  text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
+          >
+            <span class="block xl:inline font-thin">Data to enrich your</span>
+            <!-- space -->
+            <span class="block text-indigo-600 xl:inline font-normal">
+              online business</span
+            >
+          </h1>
+          <p
+            class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl font-light"
+          >
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              >
+                Get started
+              </a>
+            </div>
+            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+              >
+                Live demo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full"
+      >
+        <img
+          class="absolute object-bottom inset-0 w-full  object-contain top-24"
+          src="../assets/undraw_baby_ja7a.svg"
+          alt=""
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Hero />
+    <Feature />
+    <Cta />
+    <BlogPart />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import Header from '../components/Header.vue';
+import Hero from '../components/Hero.vue';
+import Feature from '../components/Feature.vue';
+import Cta from '../components/Cta.vue';
+import BlogPart from '../components/BlogPart.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Header,
+    Hero,
+    Feature,
+    Cta,
+    BlogPart,
+    Footer,
+  },
+};
 </script>
